5e92f24faba6e40eaedfec5c4d85be5884fe3397
Luca Borrione <luca@email.com>
2021-12-22 02:36:29 +0000

    fix: minor fixes

--- src/server/index.js
+++ src/server/index.js
@@ -16,7 +16,23 @@ app.use(express.static('dist'));
 
 app.use('/api', apiRoutes);
 
-const server = app.listen(PORT, console.log(`server running on localhost ${PORT}`)); // eslint-disable-line no-console
+// const server = app.listen(PORT, console.log(`server running on localhost ${PORT}`)); // eslint-disable-line no-console
+
+const server = app.listen(PORT, () => {
+  if (!process.env.GEONAMES_USERNAME) {
+    console.error('GEONAMES_USERNAME required');
+  }
+  if (!process.env.WEATHERBIT_APIKEY) {
+    console.error('WEATHERBIT_APIKEY required');
+  }
+  if (!process.env.PIXABAY_APIKEY) {
+    console.error('PIXABAY_APIKEY required');
+  }
+  if (!process.env.POSITIONSTACK_APIKEY) {
+    console.error('PIXABAY_APIKEY required');
+  }
+  console.log(`server running on localhost ${PORT}`);
+});
 
 module.exports = {
   app,
